9101b39503f7a8c1bb1a6a2fa2f342a2c90273ea
nikt1357 <gtoro@mail.umw.edu>
2022-09-28 10:56:24 -0400

    Added try/catch block that throws exception if the number of arguments is incorrect

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.io.FileNotFoundException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            if (args.length != 1) {
+                throw new FileNotFoundException("Incorrect number of arguments.");
+            } else {
+                // Do all the stuff
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.out.println("File could not be opened.");
+        }
+    }
 }
