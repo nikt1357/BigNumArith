30d5ac4e41d1ad10b94b3de04f8dcd3ba2b6e907
nikt1357 <gtoro@mail.umw.edu>
2022-10-12 12:28:42 -0400

    Working on reading input

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class BigNumArithmetic {
+
     public static void main(String[] args) {
         try {
             if (args.length != 1) {
@@ -12,8 +13,23 @@ public class BigNumArithmetic {
                 Scanner in = new Scanner(file);
 
                 while (in.hasNext()) {
+                    AStack stack = new AStack();
                     String s = in.nextLine();
-                    System.out.println(s);
+                    String array[] = s.split("\\s+");
+                    for(int i = 0; i < array.length; i++) {
+                        if (!array[i].equals("+") || !array[i].equals("*") || !array[i].equals("^")) {
+                            stack.push(s);
+                        } else {
+                            if (array[i].equals("+")) {
+                                Object num1 = stack.pop();
+                                Object num2 = stack.pop();
+                            } else if (array[i].equals("*")) {
+
+                            } else if (array[i].equals("^")) {
+
+                            }
+                        }
+                    }
                 }
                 in.close();
 
@@ -23,4 +39,5 @@ public class BigNumArithmetic {
             System.out.println("File could not be opened.");
         }
     }
+
 }
