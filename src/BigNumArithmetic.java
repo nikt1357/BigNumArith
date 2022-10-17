b2ff9cedaa1e40c85d3b1c69ee4a4b62cf6fec88
nikt1357 <gtoro@mail.umw.edu>
2022-10-17 01:21:25 -0400

    Revert "Multiplication mk 2"
    This reverts commit 13afa2fd37f2d89a23e6baae1c201716099a4abe.

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -45,7 +45,7 @@ public class BigNumArithmetic {
                                 AStack mult = new AStack();
                                 Object num1 = stack.pop();
                                 Object num2 = stack.pop();
-                                product = multiplication(reverse(num1), reverse(num2), mult, loop, 0, product);
+                                product = multiplication(reverse(num1), reverse(num2), mult, loop, product);
                                 String productString = "";
                                 product.moveToStart();
                                 for (int i = 0; i < product.length(); i++) {
@@ -138,7 +138,7 @@ public class BigNumArithmetic {
         return sum;
     }
 
-    public static LList multiplication(LList num1, LList num2, AStack stack, LList loop, int count, LList product) {
+    public static LList multiplication(LList num1, LList num2, AStack stack, LList loop, LList product) {
         LList top;
         LList bottom;
 
@@ -157,17 +157,18 @@ public class BigNumArithmetic {
             while (stack.length() > 1) {
                 Object first = stack.pop();
                 Object second = stack.pop();
-                loop = addition(reverse(first), reverse(second), loop);
+                product = addition(reverse(first), reverse(second), product);
                 String sumString = "";
-                loop.moveToStart();
-                for (int i = 0; i < loop.length(); i++) {
-                    sumString = sumString + loop.getValue();
-                    loop.next();
+                product.moveToStart();
+                for (int i = 0; i < product.length(); i++) {
+                    sumString = sumString + product.getValue();
+                    product.next();
                 }
                 String strPattern = "^0+(?!$)";
                 sumString = sumString.replaceAll(strPattern, "");
                 stack.push(sumString);
             }
+
             product.moveToStart();
             product.insert(stack.pop());
             return product;
@@ -215,17 +216,13 @@ public class BigNumArithmetic {
                 sumString = sumString + loop.getValue();
                 loop.next();
             }
-            for (int i = count; i > 0; i--) {
-                sumString = sumString + "0";
-            }
             String strPattern = "^0+(?!$)";
             sumString = sumString.replaceAll(strPattern, "");
             stack.push(sumString);
             bottom.moveToStart();
             bottom.remove();
             loop.clear();
-            count++;
-            product = multiplication(top, bottom, stack, loop, count, product);
+            product = multiplication(top, bottom, stack, loop, product);
         }
         return product;
     }
